510267bc02426874e6158295a3e49662fa8899c4
Joseph Kim <josepheh.kim@gmail.com>
2019-07-16 02:03:28 -0400

    finished javascript for dynamic comments

--- BandSite-Package-Sprint-2/scripts/biography.js
+++ BandSite-Package-Sprint-2/scripts/biography.js
@@ -1,19 +1,20 @@
 window.onload = function() {
+    //Global Variables
     let commentArray = [
         {
             nameObject: '1John Doe',
-            dateObject: '1mm/dd/yyyy',
-            commentObject: '1This is a test comment, get amped!'
+            commentObject: '1This is a test comment, get amped!',
+            dateObject: '1mm/dd/yyyy'
         },
         {
             nameObject: '2John Doe',
-            dateObject: '2mm/dd/yyyy',
-            commentObject: '2This is a test comment, get amped!'
+            commentObject: '2This is a test comment, get amped!',
+            dateObject: '2mm/dd/yyyy'
         },
         {
             nameObject: '3John Doe',
-            dateObject: '3mm/dd/yyyy',
-            commentObject: '3This is a test comment, get amped!'
+            commentObject: '3This is a test comment, get amped!',
+            dateObject: '3mm/dd/yyyy'
         }
     ];
 
@@ -21,11 +22,32 @@ window.onload = function() {
     let oldCommentCard = document.querySelector('.bandsite__conversation__old-comments__card');
     let commentList = document.querySelector('.form');
 
+    //Run the Old comment function    
     commentList.addEventListener('submit', retrieveInputs);
+    console.log("this is the first!");
+    
+    
+    for (let j = 0; j < commentArray.length; j++) {
+        let storedCommentCard = document.createElement('div');
+        storedCommentCard.className = 'bandsite__conversation__old-comments__card__new';
+        storedCommentCard.innerHTML = oldCommentCard.innerHTML;
 
-    //Function 1 takes object values that came from function 2 and uses them to create a new class
-    function displayComment(nameParam, dateParam, commentParam) {
-        console.log(commentArray[3]);
+        let oldUserInfo = storedCommentCard.querySelector('.bandsite__conversation__old-comments__card__text');
+        let oldUserName = oldUserInfo.querySelector('.old-user-name');
+        let oldUserComment = oldUserInfo.querySelector('.old-user-comment');
+        let oldUserDate = oldUserInfo.querySelector('.old-user-date');
+
+        oldUserName.innerHTML = `${commentArray[j].nameObject}`;
+        oldUserComment.innerHTML = `${commentArray[j].commentObject}`;
+        oldUserDate.innerHTML = `${commentArray[j].dateObject}`;
+        
+        commentSection.appendChild(storedCommentCard);
+        //console.log(commentArray[j].nameObject);
+    }
+
+    //Adds the new comment to the existing comments
+    function displayComment(nameParam, commentParam, dateParam) {
+        console.log("this is the third!");
         //Declare newCommentCard which is to add a div in comments
         let newCommentCard = document.createElement('div');
 
@@ -38,39 +60,43 @@ window.onload = function() {
         //Change default Old comments content to new user name, date, and comment
         let newUserInfo = newCommentCard.querySelector('.bandsite__conversation__old-comments__card__text');
         let newUserName = newUserInfo.querySelector('.old-user-name');
-        let newUserDate = newUserInfo.querySelector('.old-user-date');
         let newUserComment = newUserInfo.querySelector('.old-user-comment');
+        let newUserDate = newUserInfo.querySelector('.old-user-date');
 
         //adds input as an inner html
         newUserName.innerHTML = `${nameParam}`;
-        newUserDate.innerHTML = `${dateParam}`;
         newUserComment.innerHTML = `${commentParam}`;
+        newUserDate.innerHTML = `${dateParam}`;
 
         //adds the newCommentCard to commentSection
-        commentSection.appendChild(newCommentCard);
+        commentSection.insertBefore(newCommentCard, commentSection.firstChild);
     }
+    
+    //On Submit, function returns input values to array, then launches the displayComment Function
     function retrieveInputs(event) {
         event.preventDefault();
+        console.log("this is the second!");
 
-        //create a new value in the commment array, with populated sample text
+        //Declare a new object in the array, in the last position
         commentArray.push({
             nameObject: '4John Doe',
-            dateObject: '4mm/dd/yyyy',
-            commentObject: '4This is a test comment, get amped!'
+            commentObject: '4This is a test comment, get amped!',
+            dateObject: '4mm/dd/yyyy'
             }
         );
-        //x is used to find the index of last value
+
+        //x is used to find the index of the last value
         let x = commentArray.length - 1;
 
-        //finding the date
-        let today = new Date();
-        let date = (today.getMonth()+1)+'/'+today.getDate()+'/'+today.getFullYear();
-        
         //declaring input values to the new array object
-        commentArray[x].nameObject = event.target.user.value; 
-        commentArray[x].dateObject = date;
+        commentArray[x].nameObject = event.target.user.value;
         commentArray[x].commentObject = event.target.comment.value;
+        
+        //Finding the date
+        let today = new Date();
+        let date = (today.getMonth()+1)+'/'+today.getDate()+'/'+today.getFullYear();   
+        commentArray[x].dateObject = date;
 
-        displayComment(commentArray[x].nameObject, commentArray[x].dateObject, commentArray[x].commentObject);        
+        displayComment(commentArray[x].nameObject, commentArray[x].commentObject, commentArray[x].dateObject);
     }
 }
